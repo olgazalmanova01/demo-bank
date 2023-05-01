86d5c159eaf83795c371e5cd9c4cee2325a1b57a
“Olga <olgazalmanova085@gmail.com>
2023-05-01 17:37:52 +0300

    add account class

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -1,4 +1,3 @@
-import static java.lang.System.out;
 public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
@@ -10,14 +9,7 @@ public class UseAccount {
                 yourAccount.address = " 121 Outer Street ";
                         yourAccount.balance = 55.63;
                         myAccount.display();
-                        out.print(" плюс $ ");
-        out.print(myAccount.getInterest(5.00));
-        out.println(" дохода ");
+                        System.out.println();
         yourAccount.display();
-        double yourInterestRate = 7.00;
-        out.print(" плюс $ ");
-        double yourInterestAmount = yourAccount.getInterest(yourInterestRate);
-        out.print(yourInterestAmount);
-        out.println(" дохода ");
     }
 }
